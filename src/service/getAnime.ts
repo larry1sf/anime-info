8b8a02c2de21e6cc4498d78f1a2e9e2d33b38c8d
larry1sf <larryycel@gmail.com>
2026-05-03 16:07:10 -0500

    sin ruido

--- src/service/getAnime.ts
+++ src/service/getAnime.ts
@@ -503,7 +503,6 @@ export async function getAnimeById(id: number, section: string = "anime") {
     }
     return {
       ...resDefault,
-
       error: {
         message: `No se recibieron datos de la api en la url: ${url}`,
         status: 404,

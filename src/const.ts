a926f11cc3a867ad592acd64b6c240bbad8f24ef
larry1sf <larryycel@gmail.com>
2026-05-04 21:24:52 -0500

    Add const for variates components

--- src/const.ts
+++ src/const.ts
@@ -60,6 +60,71 @@ export const statusOptions = {
   ],
 };
 
+export const yearOptions = {
+  anime: [
+    { slug: "", label: "All Years" },
+    { slug: "2025", label: "2025" },
+    { slug: "2024", label: "2024" },
+    { slug: "2023", label: "2023" },
+    { slug: "2022", label: "2022" },
+    { slug: "2021", label: "2021" },
+    { slug: "2020", label: "2020" },
+    { slug: "2010s", label: "2010s" },
+    { slug: "2000s", label: "2000s" },
+    { slug: "1990s", label: "1990s" },
+  ],
+  manga: [
+    { slug: "", label: "All Years" },
+    { slug: "2025", label: "2025" },
+    { slug: "2024", label: "2024" },
+    { slug: "2023", label: "2023" },
+    { slug: "2022", label: "2022" },
+    { slug: "2021", label: "2021" },
+    { slug: "2020", label: "2020" },
+    { slug: "2010s", label: "2010s" },
+    { slug: "2000s", label: "2000s" },
+    { slug: "1990s", label: "1990s" },
+  ],
+};
+
+export const sourceOptions = {
+  anime: [
+    { slug: "", label: "All Sources" },
+    { slug: "manga", label: "Manga" },
+    { slug: "light_novel", label: "Light Novel" },
+    { slug: "visual_novel", label: "Visual Novel" },
+    { slug: "original", label: "Original" },
+    { slug: "novel", label: "Novel" },
+    { slug: "web_novel", label: "Web Novel" },
+    { slug: "game", label: "Game" },
+    { slug: "picture_book", label: "Picture Book" },
+  ],
+  manga: [
+    { slug: "", label: "All Sources" },
+    { slug: "manga", label: "Manga" },
+    { slug: "light_novel", label: "Light Novel" },
+    { slug: "novel", label: "Novel" },
+    { slug: "web_novel", label: "Web Novel" },
+    { slug: "game", label: "Game" },
+  ],
+};
+
+export const sortOptions = [
+  { slug: "", label: "Default" },
+  { slug: "desc", label: "Desc" },
+  { slug: "asc", label: "Asc" },
+];
+
+export const orderByOptions = [
+  { slug: "mal_id", label: "Default" },
+  { slug: "title", label: "Title" },
+  { slug: "score", label: "Score" },
+  { slug: "popularity", label: "Popularity" },
+  { slug: "members", label: "Members" },
+  { slug: "start_date", label: "Start Date" },
+  { slug: "end_date", label: "End Date" },
+];
+
 export const formatNumber = (num: number) => {
   if (!num) return "N/A";
   if (num >= 1000000) return (num / 1000000).toFixed(1) + "M";
